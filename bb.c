1ae460a02b5534d7fe16ede453bea260dcef8e29
Bruce Hill <bruce@bruce-hill.com>
2019-11-01 16:54:47 +0100

    Oops, I guess it was needed after all.

--- bb.c
+++ bb.c
@@ -1278,14 +1278,15 @@ int main(int argc, char *argv[])
     write(cmdfd, "\0", 1);
     for (int i = 0; i < argc; i++) {
         if (argv[i][0] == '+') {
-            char *colon = strchr(argv[i], ':');
+            char *cmd = argv[i];
+            char *colon = strchr(cmd, ':');
             if (colon && !colon[1]) {
                 for (++i; i < argc; i++) {
-                    write(cmdfd, argv[i], strlen(argv[i]));
+                    write(cmdfd, cmd, strlen(cmd));
                     write(cmdfd, argv[i], strlen(argv[i])+1); // Include null byte
                 }
             } else {
-                write(cmdfd, argv[i], strlen(argv[i])+1); // Include null byte
+                write(cmdfd, cmd, strlen(cmd)+1); // Include null byte
             }
         }
     }
