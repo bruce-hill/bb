1838fd9b9c0d8008bfe212463759449e89dd0ad3
Bruce Hill <bruce@bruce-hill.com>
2019-11-24 21:28:46 -0800

    sed change for compatibility with Mac's version of sed.

--- bbstartup.sh
+++ bbstartup.sh
@@ -5,10 +5,10 @@
 # Load key bindings
 # first check ~/.config/bb/bindings.bb, then /etc/xdg/bb/bindings.bb, then ./bindings.bb
 if [ ! -e "$XDG_CONFIG_HOME/bb/bindings.bb" ] && [ ! -e "$sysconfdir/xdg/bb/bindings.bb" ]; then
-    cat "./bindings.bb" 2>/dev/null | sed -e '/^#/d' -e "s/^[^ ]/$(printf '\034')bind:\\0/" | tr '\034' '\0' >> "$BBCMD"
+    cat "./bindings.bb" 2>/dev/null | sed -e '/^#/d' -e "s/^\([^ ]\)/$(printf '\034')bind:\\1/" | tr '\034' '\0' >> "$BBCMD"
 else
     for path in "$sysconfdir/xdg/bb" "$XDG_CONFIG_HOME/bb"; do
         cat "$path/bindings.bb" 2>/dev/null
-    done | sed -e '/^#/d' -e "s/^[^ ]/$(printf '\034')bind:\\0/" | tr '\034' '\0' >> "$BBCMD"
+    done | sed -e '/^#/d' -e "s/^\([^ ]\)/$(printf '\034')bind:\\1/" | tr '\034' '\0' >> "$BBCMD"
 fi
 printf '\0' >> "$BBCMD"
